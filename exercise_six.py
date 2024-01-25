24bb07ba66d28c73507690511770352a95056370
Angela Ferrer <gelferrero01@gmail.com>
2024-01-25 17:07:38 +0800

    Display number list

---
+++ exercise_six.py
@@ -0,0 +1,8 @@
+# Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# Create and display the number list 
+number_list = [10,20,33,46,55]
+print ('The given number list is:', number_list)
+
+# 
