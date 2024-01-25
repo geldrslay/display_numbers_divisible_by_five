f90eeb51c3147c48bb23ec70e0d4dada80bb999c
Angela Ferrer <gelferrero01@gmail.com>
2024-01-25 17:45:49 +0800

    determined errors

--- exercise_six.py
+++ exercise_six.py
@@ -1,19 +1,28 @@
 # Display numbers divisible by 5 from a list
 # Iterate the given list of numbers and print only those numbers which are divisible by 5
 
-# Defining function
-def divisible (number_list):
-    for numbers in number_list:
-        if numbers % 5 == 0:
-            print (numbers)
-
 # Display given list. 
+number_list1 = [10, 20, 33, 46, 55]
+print ('The given list is:', number_list1)
 
-number_list = [10, 20, 33, 46, 55]
-print ('The given list is:', number_list)
+# Determine and display numbers divisible by five from the list
+print ('The numbers divisible by 5 are:')
+for numbers in number_list1:
+    if numbers % 5 == 0:
+        print (numbers)
 
-# Display the numbers divisible by five from the list 
 
-print ('The numbers divisible by 5 are:', divisible(number_list))
+
+# Display another list. 
+number_list2 = [15, 21, 55, 67, 82,100]
+print ('\n''The given list is:', number_list2)
+
+# Determine and display numbers divisible by five from the list
+print ('The numbers divisible by 5 are:')
+for numbers in number_list2:
+    if numbers % 5 == 0:
+        print (numbers)
+
+
 
 
